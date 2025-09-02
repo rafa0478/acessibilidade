50efb7dab6eaafb95d50746b4c62d11521653490
rafa0478 <00001143532053SP@al.educacao.sp.gov.br>
2025-09-02 10:10:59 -0300

    a

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-document.addEventListener('DOMContentLoaded'), function(){
-    const aumentaFonteBotao = document.getElementById('aumenta-fonte');
+document.addEventListener('DOMContentLoaded', function(){
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuirFonteBotao = document.getElementById('diminuir-fonte');
 
-    let tamanhoAtualFonte=1;
+    let tamanhoAtualFonte = 1;
 
-aumentaFonteBotao.addEventListener('click', function){
-    tamanhoAtualFonte += 0.1;
-    document.body.style.fontSize = '${tamanhoAtualFonte}rem'
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    })
 
-})
-diminuirFonteBotao.addEventListener('click', function){
-    tamanhoAtualFonte -= 0.1;
-    document.body.style.fontSize = '${tamanhoAtualFonte}rem'
+     diminuirFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    })
 
-})
-    
 })
