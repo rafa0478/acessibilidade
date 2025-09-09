0a7c052f10890ce6dfa139d1f17d3097421cc41b
rafa0478 <00001143532053SP@al.educacao.sp.gov.br>
2025-09-09 10:19:52 -0300

    x

--- script.js
+++ script.js
@@ -25,6 +25,8 @@ document.addEventListener('DOMContentLoaded', function(){
 
     const alternaContraste = document.getElementById('alterna-contraste');
 
-
+alternaContraste.addEventListener('click',function(){
+    document.body.classList.toggle('alto-contraste');
+})
 
 })
